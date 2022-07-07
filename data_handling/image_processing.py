486f2a5c865847bc5ed5a9f491657a83d5604d3b
amywinder900 <amywinder98@gmail.com>
2022-07-07 20:58:29 +0100

    Adds encoder and decoder to image processing

--- data_handling/image_processing.py
+++ data_handling/image_processing.py
@@ -1,12 +1,13 @@
-from data_handling.download_from_database import DownloadTables
+from download_from_database import DownloadTables
 import pandas as pd
 import torch
-import numpy as np 
+import numpy as np
 from PIL import Image
 from alive_progress import alive_bar
 from torchvision.transforms import ToTensor
-from data_handling.clean_tabular import CleanTabular
+from clean_tabular import CleanTabular
 from os.path import isfile
+
 
 class PrepareData:
     """
@@ -15,7 +16,8 @@ class PrepareData:
         Attributes:
             crediential_location(str): Optionally, the location of the yaml file containing the database credentials. It should contain DATABASE_TYPE, DBAPI, ENDPOINT, USER, PASSWORD, PORT, DATABASE
     """
-    def __init__(self, credentials_location:str=None):
+
+    def __init__(self, credentials_location: str = None):
         """
         See help(PrepareData)
         """
@@ -24,35 +26,35 @@ class PrepareData:
         self.product_df = product_df
         self.image_df = image_df
 
-    def retrieve_dataframes(self, credentials_location:str=None):
+    def retrieve_dataframes(self, credentials_location: str = None):
         """
         Retrives the Dataframes..
 
-        
+
         Attributes:
             crediential_location(str): Optionally, the location of the yaml file containing the database credentials. It should contain DATABASE_TYPE, DBAPI, ENDPOINT, USER, PASSWORD, PORT, DATABASE
-        
+
         Returns:
             product_df(DataFrame): The clean DataFrame from the products tables. 
             image_df (DataFrame): The clean DataFrame from the images table. 
 
-        
+
         """
-        #if credentials location is given, attempt to download the tables
+        # if credentials location is given, attempt to download the tables
         if credentials_location != None:
             downloader = DownloadTables(credentials_location)
             downloader.download_table("images")
             downloader.download_table("products")
-        
+
         # check the appropriate products table exists
-        
+
         # product_table_location = "data/products_table_clean.json"
         product_table_location = "data/products_table_logistic_regression.json"
         if isfile(product_table_location) == False:
             print("Creating ", product_table_location)
             data_location = "data/products_table.json"
             product_data = pd.read_json(data_location)
-            
+
             cleaner = CleanTabular(product_data)
             cleaner.create_main_category_column()
             product_data = cleaner.get_product_df()
@@ -64,7 +66,7 @@ class PrepareData:
 
         return product_df, image_df
 
-    def retrieve_image_category(self, image_name:str) -> str:
+    def retrieve_image_category(self, image_name: str) -> str:
         """
         Given the ID of an image, retrieves the category. 
 
@@ -76,7 +78,7 @@ class PrepareData:
 
         product_row = self.product_df.loc[self.product_df["id"] == product_id]
         category = product_row.iloc[0]["main_category"]
-        
+
         return category
 
     @staticmethod
@@ -89,23 +91,20 @@ class PrepareData:
         image = torch.flatten(image)
         return image.numpy()
 
-    
     def convert_to_image_array(self, image, images_folder):
         image_category = self.retrieve_image_category(image)
-        image_location = images_folder +"/"+ image +".jpg"
+        image_location = images_folder + "/" + image + ".jpg"
         image_array = self.image_to_array(image_location)
         return image_array, image_category
 
-    
-    def create_dict_of_categories(self): 
-        categories = set(self.product_df["main_category"]) 
-        categories_dict = {k: (v - 1) for v, k in enumerate(categories)} 
-        self.categories_dict = categories_dict
-        return categories_dict
+    # def create_dict_of_categories(self):
+    #     categories = set(self.product_df["main_category"])
+    #     categories_dict = {k: (v - 1) for v, k in enumerate(categories)}
+    #     self.categories_dict = categories_dict
+    #     return categories_dict
 
+    def form_arrays(self, images_folder, image_size: int, n: int = None):
 
-    def form_arrays(self, images_folder, image_size:int, n:int = None):
-        
         images = list(self.image_df["id"])
 
         # if the number of images hasn't been specified, look at all images
@@ -115,23 +114,25 @@ class PrepareData:
         # sets up the arrays
         # array_size is based on a square image with three channels
         array_size = (image_size**2)*3
-        X = np.zeros((n,array_size))
+        X = np.zeros((n, array_size))
         y = np.zeros(n)
 
-        
-        #set up a dictionary assigning categories to integers
-        self.create_dict_of_categories()
-        
+        # #set up a dictionary assigning categories to integers
+        # self.create_dict_of_categories()
+
+        self.encoder = {y: x for (x, y) in enumerate(set(self.product_df["main_category"]))}
+        self.decoder = {x: y for (x, y) in enumerate(set(self.product_df["main_category"]))}
 
         for index in range(n):
             image = images[index]
             try:
-                features, label = self.convert_to_image_array(image, images_folder)
+                features, label = self.convert_to_image_array(
+                    image, images_folder)
                 X[index, :] = features
-                y[index] = self.categories_dict[label]
+                y[index] = self.encoder[label]
 
             except:
-                #TODO deal with this because it might be affecting the model
+                # TODO deal with this because it might be affecting the model
                 X[index, :] = np.zeros(array_size)
                 y[index] = 0
 
@@ -142,4 +143,21 @@ class PrepareData:
     def save_to_files(self, X_file_location, y_file_location):
         np.save(X_file_location, self.X)
         np.save(y_file_location, self.y)
+        np.save("data/encoder.npy", self.encoder)
+        np.save("data/decoder.npy", self.decoder)
         return None
+
+
+if __name__ == "__main__":
+
+    image_size = 124
+    credentials_location = ".gitignore/credentials_for_marketplace.yml"
+    images_folder = "data/cleaned_images_" + str(image_size)
+    X_array_folder = "data/X_for_image_"+ str(image_size) + ".npy"
+    y_array_folder = "data/y_for_image_"+ str(image_size)+".npy"
+    
+    pipeline = PrepareData()
+    product_df, image_df = pipeline.retrieve_dataframes(credentials_location)
+    X, y = pipeline.form_arrays(images_folder, image_size, n=10 )
+    pipeline.save_to_files(X_array_folder, y_array_folder)
+    pass
