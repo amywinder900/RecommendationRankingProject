fcd5abd11f33bfc0b6ebd7c018c36de727b8e1ac
amywinder900 <amywinder98@gmail.com>
2022-07-28 20:49:14 +0100

    Refactors image model code

--- image_loader.py
+++ image_loader.py
@@ -1,16 +1,8 @@
-#%%
-import copy
 import numpy as np
 import pandas as pd
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import os
-from os.path import isfile
-from torch.optim import lr_scheduler
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
-from torch.utils.tensorboard import SummaryWriter
-from torchvision import models
 from torchvision import transforms
 from PIL import Image
 
@@ -22,13 +14,13 @@ class ProductImageCategoryDataset(Dataset):
 
     Parameters:
 
-    X(numpy.ndarray): The image data as a numpy array.
+        images_location(str): The folder location containing the cleaned images.
 
-    y(numpy.ndarray): The image category data as a numpy array. 
+        img_side_length(int): The side length of the images. 
 
-    img_side_length(int): The side length of the images. 
+        load_image_category_table(bool): If true, the class loads the image category table from data/image_category_table.json or if False it will create a new one. Default value is False.
 
-    transform: The transformation or list of transformations to be done to the image. If no transform is passed, the class will do a generic transformation to resize, convert it to a tensor, and normalize the numbers
+        transform: The transformation or list of transformations to be done to the image. If no transform is passed, the class will do a generic transformation to resize, convert it to a tensor, and normalize the numbers
 
 
     """
@@ -42,7 +34,7 @@ class ProductImageCategoryDataset(Dataset):
         super().__init__()
 
         self.img_side_length = img_side_length
-        
+
         # Check image file exists
         if not os.path.exists(images_location):
             raise RuntimeError("Image Dataset not found at: ", images_location)
@@ -50,7 +42,8 @@ class ProductImageCategoryDataset(Dataset):
 
         # Load or create the image category table
         if load_image_category_table:
-            self.image_category_table = pd.read_json("data/image_category_table.json")
+            self.image_category_table = pd.read_json(
+                "data/image_category_table.json")
         else:
             self.image_category_table = self.create_image_category_table()
 
@@ -58,8 +51,8 @@ class ProductImageCategoryDataset(Dataset):
         self.image_ids = self.image_category_table["image_id"]
         self.category_labels = self.image_category_table["main_category"]
         assert len(self.image_ids) == len(self.category_labels)
-        
-        #Create label encoder/decoder
+
+        # Create label encoder/decoder
         self.labels = self.image_category_table['main_category'].to_list()
         self.encoder = {y: x for (x, y) in enumerate(set(self.labels))}
         self.decoder = {x: y for (x, y) in enumerate(set(self.labels))}
@@ -67,10 +60,10 @@ class ProductImageCategoryDataset(Dataset):
         self.transform = transform
         if transform is None:
             self.transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     @staticmethod
     def create_image_category_table():
@@ -82,38 +75,74 @@ class ProductImageCategoryDataset(Dataset):
         return products_images
 
     def __getitem__(self, index: int):
-        
-        # features = self.X[index]
-        # features = torch.tensor(features)
-        # features = features.reshape(
-        #     3, self.img_side_length, self.img_side_length)
-        # features = transforms.ToPILImage()(features)
-        # if self.transform:
-        #     features = self.transform(features)
 
         label = self.category_labels[index]
         label = self.encoder[label]
         label = torch.as_tensor(label)
-        image = Image.open(self.images_location + self.image_ids[index] +".jpg")
+        image = Image.open(self.images_location +
+                           self.image_ids[index] + ".jpg")
+        image = self.transform(image)
         return image, label
 
     def __len__(self):
         return len(self.image_ids)
 
 
-if __name__ == '__main__':
-    dataset = ProductImageCategoryDataset("data/cleaned_images_128/", 128)
-    print(dataset[0])
-    print(dataset.decoder[int(dataset[0][1])])
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=12,
-                                             shuffle=True, num_workers=1)
-    for i, (data, labels) in enumerate(dataloader):
-        print(data)
-        print(labels)
-        print(data.size())
-        if i == 0:
-            break
+def create_data_loaders(images_location: str,
+                        image_dataset:Dataset,  
+                        image_size:int, 
+                        image_transforms:dict, 
+                        validation_split:float=0.2, 
+                        batch_size:int=16, 
+                        shuffle:bool=True):
+    """
+    This function creates the dataloaders with a training and validation split. 
 
-# %%
+    Parameters:
 
-# %%
+      X(numpy.ndarray): The image data as a numpy array.
+
+      y(numpy.ndarray): The image category data as a numpy array. 
+
+      image_dataset(Dataset): The dataset object which prepares the image and the category label. 
+
+      image_side_length(int): The side length of the images. 
+
+      image_transforms(dict): A dictionary containing the the transformation or list of transformations to be done to the image. It should contain transformations for the train and validation phases using the keys "train" and "val". 
+
+      validation_split(float): The proportion of the dataset which should be used for validation. Default is 0.2
+
+      batch_size(int): The batch size to process the images in. 
+
+      shuffle(bool): Whether or not to shuffle the order of the images. Default is true.
+
+
+    """
+    dataset_length = len(image_dataset(images_location, image_size))
+
+    # find split
+    split_indice = int(np.floor(validation_split * dataset_length))
+
+    # create list of indices and shuffle
+    indices = list(range(dataset_length))
+    if shuffle:
+        np.random.shuffle(indices)
+
+    # Create dictionary of dataset sizes
+    dataset_sizes = {"train": dataset_length - int(np.floor(validation_split * dataset_length)),
+                     "val": int(np.floor(validation_split * dataset_length))}
+
+    # Create sampler
+    sampler = {"train": SubsetRandomSampler(indices[split_indice:]),
+               "val": SubsetRandomSampler(indices[:split_indice])}
+
+    # Form datasets
+    dataset = {phase: image_dataset(images_location, image_size, transform=image_transforms[phase])
+               for phase in ["train", "val"]}
+
+    # Load data for each phase
+    data_loader = {phase: DataLoader(dataset[phase], batch_size=batch_size, sampler=sampler[phase])
+                   for phase in ["train", "val"]}
+
+    return data_loader, dataset_sizes
+
